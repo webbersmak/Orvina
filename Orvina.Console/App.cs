5ef27caaed4888fff591d6da196718c8292c64d4
developer1 <dclemens@gmx.com>
2022-02-23 17:17:11 -0500

    Add project files.

---
+++ Orvina.Console/App.cs
@@ -0,0 +1,289 @@
+﻿using Orvina.Engine;
+using System.Diagnostics;
+
+namespace Orvina.Console
+{
+    internal class App
+    {
+        private readonly Dictionary<int, string> fileMap = new();
+        private readonly AppState state;
+
+        private readonly Stopwatch stopwatch = new();
+
+        private bool attemptQuit;
+
+        private string[] fileExtensions;
+
+        private int fileId = 1;
+        private bool includeSubdirectories;
+
+        private int searchCount = 0;
+
+        private bool searchEnded;
+
+        private string searchPath;
+
+        private string searchText;
+
+        private bool showErrors;
+
+        public App(string[] args)
+        {
+            state = ProcessArgs(args);
+        }
+
+        private enum AppState
+        {
+            ShowHelp,
+            Run
+        }
+
+        public void Run()
+        {
+            switch (state)
+            {
+                case AppState.ShowHelp:
+                    WriteLine("Quickly find files containing the desired text.\n");
+                    WriteLine("Usage:\n");
+                    WriteLine("orvina <search path> <search text> [file extensions] [-nosub]\n");
+                    WriteLine("     <search path>   Specifies the directory to search");
+                    WriteLine("     <search text>   Declares the text to search for in the files");
+                    WriteLine("     [file extensions]   Comma separated list of file extensions. Restricts searching to specific file types.");
+                    WriteLine("                         Optional. Use to restrict search to specific file types.");
+                    WriteLine("     -nosub              If given, do not search subdirectories in the search path.");
+                    WriteLine("     -debug              If given, show error messages.");
+                    WriteLine("");
+                    WriteLine("Example:\n");
+                    WriteLine("fileeye \"C:\\my files\" \"return 1\"  \".cs,.js\"");
+                    break;
+
+                case AppState.Run:
+                    DoSearch();
+                    break;
+            }
+        }
+
+        private static string ConsoleTruncate(string text)
+        {
+            var width = System.Console.WindowWidth - 1;
+            return text.Length > width ? text.Substring(0, width) : text;
+        }
+
+        private static void PrintWipe(string text)
+        {
+            text = ConsoleTruncate(text);
+            SetColor(ConsoleColor.Green);
+
+            System.Console.Write($"\r{text}".PadRight(System.Console.BufferWidth));
+
+            //System.Console.Write($"\r{consoleClear}");
+            //System.Console.Write($"\r{text}");
+        }
+
+        private static void SetColor(ConsoleColor color)
+        {
+            System.Console.ForegroundColor = color;
+        }
+
+        private static List<string> SplitAndKeepDelimiter(string input, string delimiter)
+        {
+            var parts = new List<string>();
+
+            int startIdx;
+            while ((startIdx = input.IndexOf(delimiter, StringComparison.OrdinalIgnoreCase)) >= 0)
+            {
+                if (startIdx > 0)
+                {
+                    parts.Add(input.Substring(0, startIdx));
+                    input = input.Remove(0, startIdx);
+                }
+                else //if (startIdx == 0)
+                {
+                    parts.Add(input.Substring(0, delimiter.Length));
+                    input = input.Remove(0, delimiter.Length);
+                }
+            }
+
+            if (input.Length > 0) //any leftover
+            {
+                parts.Add(input);
+            }
+
+            return parts;
+        }
+
+        private static void WriteLine(string msg)
+        {
+            System.Console.WriteLine(msg);
+        }
+
+        private void DoSearch()
+        {
+            using (var search = new SearchEngine())
+            {
+                search.OnError += Search_OnError;
+                search.OnFileFound += Search_OnFileFound;
+                search.OnSearchComplete += Search_OnSearchComplete;
+                search.OnProgress += Search_OnProgress;
+
+                WriteLine("searching...('q' to quit)\n");
+                stopwatch.Start();
+                search.Start(searchPath, includeSubdirectories, searchText, fileExtensions);
+
+                while (!searchEnded)
+                {
+                    if (System.Console.KeyAvailable
+                        && System.Console.ReadKey(true).Key == ConsoleKey.Q)
+                    {
+                        search.Stop();
+                        attemptQuit = true;
+                    }
+
+                    Thread.Sleep(100);
+                }
+
+                var sawAFile = fileId > 1;
+                if (!attemptQuit && sawAFile)
+                {
+                    WriteLine("Open File? Enter Id or 'q' to quit: ");
+
+                    var sawQ = false;
+                    var fileOpened = false;
+
+                    while (!sawQ && !fileOpened)
+                    {
+                        while (!System.Console.KeyAvailable)
+                        {
+                            Thread.Sleep(100);
+                        }
+
+                        var key = System.Console.ReadKey(true);
+                        sawQ = key.Key == ConsoleKey.Q;
+
+                        if (!sawQ && char.IsNumber(key.KeyChar))
+                        {
+                            var fileId = key.KeyChar + System.Console.ReadLine();
+
+                            int result;
+                            if (int.TryParse(fileId, out result))
+                            {
+                                if (fileMap.ContainsKey(result))
+                                {
+                                    fileOpened = true;
+                                    var file = fileMap[result];
+
+                                    Process.Start(new ProcessStartInfo(file) { UseShellExecute = true });
+                                }
+                            }
+                        }
+                    }
+                }
+
+                //being a gentleman
+                search.OnError -= Search_OnError;
+                search.OnFileFound -= Search_OnFileFound;
+                search.OnSearchComplete -= Search_OnSearchComplete;
+                search.OnProgress -= Search_OnProgress;
+            }
+        }
+
+        private AppState ProcessArgs(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1) //probably -help flag
+                {
+                    switch (args[0])
+                    {
+                        case "-h":
+                        case "-help":
+                        case "/H":
+                        case "/h":
+                            return AppState.ShowHelp;
+                    }
+                }
+                else
+                {
+                    var nosubFlag = args.Any(a => a == "-nosub" || a == "/nosub");
+                    var debugFlag = args.Any(a => a == "-debug" || a == "/debug");
+
+                    searchPath = args[0];
+                    searchText = args[1];
+                    fileExtensions = args.Length < 3 //not enough args to consider this
+                        || (args.Length == 3 && !nosubFlag && !debugFlag) 
+                        || (args.Length == 4 && nosubFlag && debugFlag)
+                         ? new string[] { } : args[2].Split(',');
+                    includeSubdirectories = !nosubFlag;
+                    showErrors = debugFlag;
+                    return AppState.Run;
+                }
+            }
+            catch
+            {
+            }
+
+            return AppState.ShowHelp;
+        }
+
+        private void Search_OnError(string error)
+        {
+            if (showErrors)
+            {
+                SetColor(ConsoleColor.Red);
+                WriteLine($"\r{error}");
+            }
+        }
+
+        private void Search_OnFileFound(string file, List<string> matchingLines)
+        {
+            PrintWipe("");
+
+            var fileParts = SplitAndKeepDelimiter(file, @"\");
+
+            SetColor(ConsoleColor.Green);
+            System.Console.Write($"\rFound: " + string.Join("", fileParts.Select(p => p == fileParts.Last() ? "" : p)));
+            SetColor(ConsoleColor.Red);
+            System.Console.Write(fileParts.Last());
+
+            SetColor(ConsoleColor.DarkGray);
+            WriteLine($"({fileId})");
+
+            fileMap.Add(fileId++, file);
+
+            SetColor(ConsoleColor.Green);
+            foreach (string line in matchingLines.Select(line => ConsoleTruncate(line)))
+            {
+                var parts = SplitAndKeepDelimiter(line, searchText);
+                foreach (var part in parts)
+                {
+                    SetColor(part.Equals(searchText, StringComparison.OrdinalIgnoreCase)
+                        ? ConsoleColor.DarkCyan
+                        : ConsoleColor.Yellow);
+                    System.Console.Write(ConsoleTruncate(part));
+                }
+
+                WriteLine("");
+
+                if (attemptQuit)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Search_OnProgress(string filePath)
+        {
+            searchCount++;
+            PrintWipe(filePath);
+        }
+
+        private void Search_OnSearchComplete()
+        {
+            PrintWipe("");
+            stopwatch.Stop();
+            SetColor(ConsoleColor.Gray);
+            WriteLine($"\nSearch Complete! {searchCount} Files searched in {Math.Round(stopwatch.Elapsed.TotalSeconds, 2)}s.\n");
+            searchEnded = true;
+        }
+    }
+}
---
+++ Orvina.Engine/SearchEngine.cs
@@ -0,0 +1,261 @@
+﻿namespace Orvina.Engine
+{
+    public class SearchEngine : IDisposable
+    {
+        private readonly object diskLock = new();
+        private readonly object endLock = new();
+        private readonly List<string> files = new();
+        private readonly object notificationLock = new();
+        private readonly List<Task> tasks = new();
+
+        private List<string> fileExtensions;
+
+        private int taskCount;
+        private bool searchEnded;
+
+        private volatile int finishedTasks;
+        private volatile bool listingDirectory;
+        private volatile bool stop;
+
+        /// <summary>
+        /// returns error message
+        /// </summary>
+        public event Action<string> OnError;
+
+        /// <summary>
+        /// returns the file and the lines in the file that match the search
+        /// </summary>
+        public event Action<string, List<string>> OnFileFound;
+
+        /// <summary>
+        /// returns the currently scanned file
+        /// </summary>
+        public event Action<string> OnProgress;
+
+        /// <summary>
+        /// this will be raised when the search is complete. Expect this event even if Stop() is called
+        /// </summary>
+        public event Action OnSearchComplete;
+
+        /// <summary>
+        /// Safe to call after the OnSearchComplete event is raised.
+        /// </summary>
+        public void Dispose()
+        {
+            if (searchEnded)
+            {
+                //saw a bug (windows?) where a completed thread still showed as running
+                //this while() will let threads finish up if they needed a few
+                //extra microseconds to update their status
+                while (tasks.Any(t => t.Status == TaskStatus.Running)) { }
+                tasks.ForEach(t => { t.Dispose(); });
+            }
+        }
+
+        /// <summary>
+        /// Call this method to start the search. Or call it to restart the search after the OnSearchComplete event is raised.
+        /// </summary>
+        /// <param name="searchPath">such as @"C:\my files"</param>
+        /// <param name="includeSubirectories">true or false</param>
+        /// <param name="searchText">the text the file should contain. Not case sensitive. Not a regular expression (yet)</param>
+        /// <param name="fileExtensions">such as ".cs", ".txt"</param>
+        public void Start(string searchPath, bool includeSubirectories, string searchText, params string[] fileExtensions)
+        {
+            if (tasks.Any(t => t.Status != TaskStatus.RanToCompletion))
+            {
+                //looks like the search is already running...
+                return;
+            }
+            else
+            {
+                //clean up tasks from previous run
+                Dispose();
+                tasks.Clear();
+            }
+
+            stop = false;
+            searchEnded = false;
+            finishedTasks = 0;
+            listingDirectory = true;
+
+            this.fileExtensions = fileExtensions.ToList();
+            this.taskCount = Environment.ProcessorCount > 1 ? Environment.ProcessorCount : 2;
+
+            tasks.Add(Task.Run(() =>
+            {
+                ListDirectory(searchPath, includeSubirectories);
+                listingDirectory = false;
+                TryNotifySearchEnded();
+            }));
+
+            for (var i = 1; i < taskCount; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    SearchFile(searchText);
+                    TryNotifySearchEnded();
+                }));
+            }
+        }
+
+        /// <summary>
+        /// Request the search to stop. There may be a delay between calling this method
+        /// and seeing the OnSearchComplete event
+        /// </summary>
+        public void Stop()
+        {
+            stop = true; //let threads run to completion
+        }
+
+        private void ListDirectory(string path, bool includeSubirectories)
+        {
+            if (stop)
+            {
+                return;
+            }
+
+            lock (notificationLock)
+                OnProgress?.Invoke(path);
+
+            try
+            {
+                var filesToAdd = Directory.GetFiles(path).Where(file => !fileExtensions.Any() || fileExtensions.Any(t => file.EndsWith(t))).ToList();
+                if (filesToAdd.Any())
+                {
+                    lock (files)
+                    {
+                        files.AddRange(filesToAdd);
+                    }
+                }
+
+                if (includeSubirectories)
+                {
+                    foreach (var dir in Directory.GetDirectories(path))
+                    {
+                        ListDirectory(dir, includeSubirectories);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                lock (notificationLock)
+                    OnError?.Invoke(e.ToString());
+            }
+        }
+
+        private void SearchFile(string searchText)
+        {
+            int fileCount;
+            string target;
+
+            var matchingLines = new List<string>();
+
+            do
+            {
+                target = "";
+
+                lock (files)
+                {
+                    fileCount = files.Count;
+                    if (fileCount > 0)
+                    {
+                        target = files[0];
+                        files.RemoveAt(0);
+                        fileCount--;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(target))
+                {
+                    try
+                    {
+                        lock (notificationLock)
+                            OnProgress?.Invoke(Path.GetFileName(target));
+
+                        try //bulk read
+                        {
+                            string all;
+                            lock (diskLock) //get on/off disk ASAP
+                            {
+                                all = File.ReadAllText(target);
+                            }
+
+                            //most cases the file won't contain the searchText at all
+                            if (all.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                            {
+                                var lineNum = 0;
+                                foreach (var line in all.Split("\n"))
+                                {
+                                    lineNum++;
+                                    if (line.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                                    {
+                                        matchingLines.Add($"({lineNum}) " + line);
+                                    }
+
+                                    if (stop)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        catch (OutOfMemoryException)
+                        {
+                            try //line by line
+                            {
+                                using (TextReader reader = new StreamReader(target))
+                                {
+                                    string line;
+                                    do
+                                    {
+                                        line = reader.ReadLine();
+                                        if (line != null && line.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                                        {
+                                            matchingLines.Add(line);
+                                        }
+
+                                        if (stop)
+                                        {
+                                            break;
+                                        }
+                                    } while (line != null);
+                                }
+                            }
+                            catch
+                            {
+                                throw;
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        lock (notificationLock)
+                            OnError?.Invoke(e.ToString());
+                    }
+
+                    if (matchingLines.Any())
+                    {
+                        lock (notificationLock)
+                            OnFileFound?.Invoke(target, matchingLines);
+
+                        matchingLines.Clear();
+                    }
+                }
+            } while (!stop && (fileCount > 0 || listingDirectory));
+        }
+
+        private void TryNotifySearchEnded()
+        {
+            lock (endLock)
+            {
+                finishedTasks++;
+                if (!searchEnded && finishedTasks == taskCount)
+                {
+                    searchEnded = true;
+                    lock (notificationLock)
+                        OnSearchComplete?.Invoke();
+                }
+            }
+        }
+    }
+}
